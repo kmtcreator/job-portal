824dd0c33a21bb3b961158f5f155e3a9b5225ca6
kmtcreator <145428074+kmtcreator@users.noreply.github.com>
2023-09-27 14:16:41 +0630

    app.js

--- app.js
+++ app.js
@@ -1,49 +1,30 @@
-// Initialize Firebase (use your own Firebase configuration)
-const firebaseConfig = {
-    apiKey: "AIzaSyCTHc5uV9qLcOmpo_kZBAWs0DJ8dLPkxhE",
-    authDomain: "recruitmentcv-fe4aa.firebaseapp.com",
-    projectId: "recruitmentcv-fe4aa",
+// Sample data for CV (you can replace this with dynamic data from a database)
+const cvData = {
+    name: "John Doe",
+    email: "johndoe@example.com",
+    education: "Bachelor's in Computer Science",
+    experience: "Software Engineer at XYZ Inc.",
+    skills: "JavaScript, HTML, CSS, React, Node.js",
+    // Add more fields as needed
 };
-firebase.initializeApp(firebaseConfig);
 
-// Reference to Firebase Authentication
-const auth = firebase.auth();
+// Function to display CV data
+function displayCV() {
+    const cvDisplay = document.getElementById("cv-display");
 
-// DOM elements
-const loginEmail = document.getElementById("login-email");
-const loginPassword = document.getElementById("login-password");
-const loginButton = document.getElementById("login-button");
-const signupEmail = document.getElementById("signup-email");
-const signupPassword = document.getElementById("signup-password");
-const signupButton = document.getElementById("signup-button");
-const errorMessage = document.getElementById("error-message");
+    const cvHTML = `
+        <h2>${cvData.name}</h2>
+        <p>Email: ${cvData.email}</p>
+        <h3>Education</h3>
+        <p>${cvData.education}</p>
+        <h3>Experience</h3>
+        <p>${cvData.experience}</p>
+        <h3>Skills</h3>
+        <p>${cvData.skills}</p>
+    `;
 
-// Log In event listener
-loginButton.addEventListener("click", () => {
-    const email = loginEmail.value;
-    const password = loginPassword.value;
-    auth.signInWithEmailAndPassword(email, password)
-        .then(() => {
-            // Successfully logged in
-            errorMessage.textContent = "";
-            window.location.href = "inbox.html"; // Redirect to inbox or dashboard
-        })
-        .catch((error) => {
-            errorMessage.textContent = error.message;
-        });
-});
+    cvDisplay.innerHTML = cvHTML;
+}
 
-// Sign Up event listener
-signupButton.addEventListener("click", () => {
-    const email = signupEmail.value;
-    const password = signupPassword.value;
-    auth.createUserWithEmailAndPassword(email, password)
-        .then(() => {
-            // Successfully signed up
-            errorMessage.textContent = "";
-            window.location.href = "inbox.html"; // Redirect to inbox or dashboard
-        })
-        .catch((error) => {
-            errorMessage.textContent = error.message;
-        });
-});
+// Initial display of CV
+displayCV();
